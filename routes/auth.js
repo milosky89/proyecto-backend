c20031302ddd06812df016750693efd4024e1aa8
milosky89 <camilo77543@hotmail.com>
2022-09-14 20:07:50 -0500

    renewtoken

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,10 @@
     path: /api/login
 */
 const { Router } = require('express');
-const { login } = require('../controllers/auth');
+const { login,renewToken } = require('../controllers/auth');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
@@ -18,5 +19,9 @@ router.post( '/',
     login
 )
 
+router.get( '/renew',
+    validarJWT,
+    renewToken
+)
 
 module.exports = router;
