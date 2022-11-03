bfc5cbcf488fafb6b4a8095bc931581f865c8db0
milosky89 <camilo77543@hotmail.com>
2022-11-02 22:44:51 -0500

    Api analisis grafico completo

--- controllers/analisis.js
+++ controllers/analisis.js
@@ -86,7 +86,7 @@ const getData = async (req, res = response) => {
                 resultadoV
             });
             break;
-        case 'Tipo alimentación':
+        case 'Tipo de Alimentacion':
             var resultadoT = []
             for (var i = 0; i < filtroComunas.length; i++) {
                 const perroCO = await Mascota.find({ mascota: 'perro', comuna: filtroComunas[i], tipoAlimentacion: 'Concentrado' }).countDocuments();
@@ -95,21 +95,124 @@ const getData = async (req, res = response) => {
                 const gatoCA = await Mascota.find({ mascota: 'gato', comuna: filtroComunas[i], tipoAlimentacion: 'Casero' }).countDocuments();
                 resultadoT.push({ comuna: filtroComunas[i], perroCO: perroCO, perroCA: perroCA, gatoCO: gatoCO, gatoCA: gatoCA })
             }
+            var final = []
+            var final2 = []
+            final = resultadoT[0]
+
             res.json({
-                resultadoT
+                final
             });
             break;
 
         default:
             console.log(`Sorry, we are out of ${consulta}.`);
     }
+}
+const getDatas = async (req, res = response) => {
+    const comuna = req.query.comuna
+    const consulta = req.query.consulta
+    switch (consulta) {
+        case 'Tipo de mascota':
+            const perro = await Mascota.find({ mascota: 'perro', comuna: comuna }).countDocuments();
+            const gato = await Mascota.find({ mascota: 'gato', comuna: comuna }).countDocuments();
 
+            res.json({
+                perro,
+                gato
+            });
+            break;
+        case 'Tipo de Alimentacion':
+            const perroCO = await Mascota.find({ mascota: 'perro', comuna: comuna, tipoAlimentacion: 'Concentrado' }).countDocuments();
+            const perroCA = await Mascota.find({ mascota: 'perro', comuna: comuna, tipoAlimentacion: 'Casero' }).countDocuments();
+            const gatoCO = await Mascota.find({ mascota: 'gato', comuna: comuna, tipoAlimentacion: 'Concentrado' }).countDocuments();
+            const gatoCA = await Mascota.find({ mascota: 'gato', comuna: comuna, tipoAlimentacion: 'Casero' }).countDocuments();
+            res.json({
+                perroCO,
+                perroCA,
+                gatoCO,
+                gatoCA
+            });
+            break;
+        case 'Sexo':
+            const perroM = await Mascota.find({ mascota: 'perro', comuna: comuna, sexo: 'Macho' }).countDocuments();
+            const perroH = await Mascota.find({ mascota: 'perro', comuna: comuna, sexo: 'Hembra' }).countDocuments();
+            const gatoM = await Mascota.find({ mascota: 'gato', comuna: comuna, sexo: 'Macho' }).countDocuments();
+            const gatoH = await Mascota.find({ mascota: 'gato', comuna: comuna, sexo: 'Hembra' }).countDocuments();
+            res.json({
+                perroM,
+                perroH,
+                gatoM,
+                gatoH
+            });
+            break;
+        case 'Adquisición':
+            const perroC = await Mascota.find({ mascota: 'perro', comuna: comuna, adquisicion: 'Compra' }).countDocuments();
+            const perroA = await Mascota.find({ mascota: 'perro', comuna: comuna, adquisicion: 'Adopción' }).countDocuments();
+            const gatoC = await Mascota.find({ mascota: 'gato', comuna: comuna, adquisicion: 'Compra' }).countDocuments();
+            const gatoA = await Mascota.find({ mascota: 'gato', comuna: comuna, adquisicion: 'Adopción' }).countDocuments();
+            res.json({
+                perroC,
+                perroA,
+                gatoC,
+                gatoA
+            });
+            break;
+        case 'Esterilizacion':
+            const perroS = await Mascota.find({ mascota: 'perro', comuna: comuna, esterilizacion: 'Si' }).countDocuments();
+            const perroN = await Mascota.find({ mascota: 'perro', comuna: comuna, esterilizacion: 'No' }).countDocuments();
+            const gatoS = await Mascota.find({ mascota: 'gato', comuna: comuna, esterilizacion: 'Si' }).countDocuments();
+            const gatoN = await Mascota.find({ mascota: 'gato', comuna: comuna, esterilizacion: 'No' }).countDocuments();
 
+            res.json({
+                perroS,
+                perroN,
+                gatoS,
+                gatoN
+            });
+            break;
+        case 'Esquema de vacunación':
+            const perroSi = await Mascota.find({ mascota: 'perro', comuna: comuna, vacunacion: 'Si' }).countDocuments();
+            const perroNo = await Mascota.find({ mascota: 'perro', comuna: comuna, vacunacion: 'No' }).countDocuments();
+            const gatoSi = await Mascota.find({ mascota: 'gato', comuna: comuna, vacunacion: 'Si' }).countDocuments();
+            const gatoNo = await Mascota.find({ mascota: 'gato', comuna: comuna, vacunacion: 'No' }).countDocuments();
 
+            res.json({
+                perroSi,
+                perroNo,
+                gatoSi,
+                gatoNo
+
+            });
+            break;
+        case 'Estado actual':
+            const perroV = await Mascota.find({ mascota: 'perro', comuna: comuna, estado: 'Vivo' }).countDocuments();
+            const perroF = await Mascota.find({ mascota: 'perro', comuna: comuna, estado: 'Muerto' }).countDocuments();
+            const perroP = await Mascota.find({ mascota: 'perro', comuna: comuna, estado: 'Perdido' }).countDocuments();
+            const perroAd = await Mascota.find({ mascota: 'perro', comuna:comuna, estado: 'En Adopción' }).countDocuments();
+            const gatoV = await Mascota.find({ mascota: 'gato', comuna: comuna, estado: 'vivo' }).countDocuments();
+            const gatoF = await Mascota.find({ mascota: 'gato', comuna: comuna, estado: 'Muerto' }).countDocuments();
+            const gatoP = await Mascota.find({ mascota: 'gato', comuna: comuna, estado: 'Perdido' }).countDocuments();
+            const gatoAd = await Mascota.find({ mascota: 'gato', comuna: comuna, estado: 'En Adopción' }).countDocuments();
+            res.json({
+                perroV,
+                perroF,
+                perroP,
+                perroAd,
+                gatoV,
+                gatoF,
+                gatoP,
+                gatoAd
+            });
+            break;
+        default:
+            console.log(`Sorry, we are out of ${consulta}.`);
+    }
 }
 
 
 
+
 module.exports = {
-    getData
+    getData,
+    getDatas
 }
--- routes/analisis.js
+++ routes/analisis.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { getData } = require('../controllers/analisis');
+const { getData,getDatas } = require('../controllers/analisis');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 
@@ -7,5 +7,6 @@ const { validarJWT } = require('../middlewares/validar-jwt');
 const router = Router();
 
 router.get('/analisis/', getData);
+router.get('/analisis2', getDatas);
 
 module.exports = router;
